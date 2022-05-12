304be1c34a9a7c9c5c5c87083ed58103cfa83fc0
Jerry Yu <jerryyu77@gmail.com>
2022-05-12 14:28:20 -0400

    adding additional button functionality and styling

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,27 +6,39 @@ function Calculator() {
   const operand1 = useRef("");
   const operand2 = useRef("");
   const operator = useRef("");
+  const appendOn = useRef(true);
   const calcTotal = () => {
       let total;
 
       switch(operator.current) {
         case "+":
-          total = parseInt(operand1.current) + parseInt(operand2.current || input);
+          total = parseFloat(operand1.current) + parseFloat(operand2.current || input);
           break;
         case "-":
-          total = parseInt(operand1.current) - parseInt(operand2.current || input);
+          total = parseFloat(operand1.current) - parseFloat(operand2.current || input);
           break;
         case "x":
-          total = parseInt(operand1.current) * parseInt(operand2.current || input);
+          total = parseFloat(operand1.current) * parseFloat(operand2.current || input);
           break;
         case "/":
-          total = parseInt(operand1.current) / parseInt(operand2.current || input);
+          total = parseFloat(operand1.current) / parseFloat(operand2.current || input);
+          break;
+        default:
           break;
       }
 
-      return total;
+      return total.toString();
   };
+  const settingInput = (value) => {
+    let truncateLength = 17;
+    
+    if (value.length > truncateLength) {
+      value = value.substring(0, truncateLength);
+    }
+    setInput(value);
+  }
   const buttonClick = (e) => {
+    let num;
     switch(e.target.innerHTML) {
       case "C":
         setInput("0");
@@ -34,12 +46,14 @@ function Calculator() {
         operand2.current = "";
         operator.current = "";
         break;
-      case "()":
-        console.log(e.target.innerHTML);
+      case "x^2":
+        settingInput((parseFloat(input) ** 2).toString());
         break;
       case "%":
+        num = parseFloat(input);
+        settingInput((num/100).toString());
         break;
-      case "*":
+      case "x":
       case "/":
       case "+":
       case "-":
@@ -49,22 +63,36 @@ function Calculator() {
         } else if (operand1.current) {
           let total = calcTotal();
 
-          setInput(total);
+          settingInput(total);
           operand1.current = total;
         } else {
           operand1.current = input;
         }
         operator.current = e.target.innerHTML;
+        appendOn.current = false;
         break;
       case "=":
         if (operand1.current) {
           let total = calcTotal();
           
           operand2.current = operand2.current || input;
-          setInput(total);
+          settingInput(total);
           operand1.current = total;
         } else {
           operand1.current = input;
+        }
+        appendOn.current = false;
+        break;
+      case ".":        
+        if (!input.includes(".")) {
+          settingInput(input + ".");
+        }
+        break;
+      case "+/-":
+        if (input.includes("-")) {
+          setInput(input.slice(1));
+        } else {
+          setInput("-" + input);
         }
         break;
       case "1":
@@ -77,53 +105,55 @@ function Calculator() {
       case "8":
       case "9":
       case "0":
-        let num = e.target.innerHTML;
+        num = e.target.innerHTML;
         
-        if (input === "0" || operator.current) {
+        if (input === "0" || !appendOn.current) {
           setInput(num);
+          appendOn.current = true;
         } else {
           setInput(input + num);
         }
+        break;
+      default:
         break;
     }    
   };
 
   return (
-    <div className="Calculator">
+    <div className="calculator-container">
       <div>
         <div className="row">
-          <input value={input}>
-          </input>
+          <input value={input} readOnly={true}/>
         </div>
         <div className="row">
           <button onClick={buttonClick}>C</button>
-          <button onClick={buttonClick}>()</button>
+          <button onClick={buttonClick}>x^2</button>
           <button onClick={buttonClick}>%</button>
           <button onClick={buttonClick}>/</button>
         </div>
         <div className="row">
-          <button onClick={buttonClick}>7</button>
-          <button onClick={buttonClick}>8</button>
-          <button onClick={buttonClick}>9</button>
+          <button className="num-pad" onClick={buttonClick}>7</button>
+          <button className="num-pad" onClick={buttonClick}>8</button>
+          <button className="num-pad" onClick={buttonClick}>9</button>
           <button onClick={buttonClick}>x</button>
         </div>
         <div className="row">
-          <button onClick={buttonClick}>4</button>
-          <button onClick={buttonClick}>5</button>
-          <button onClick={buttonClick}>6</button>
+          <button className="num-pad" onClick={buttonClick}>4</button>
+          <button className="num-pad" onClick={buttonClick}>5</button>
+          <button className="num-pad" onClick={buttonClick}>6</button>
           <button onClick={buttonClick}>-</button>
         </div>
         <div className="row">
-          <button onClick={buttonClick}>1</button>
-          <button onClick={buttonClick}>2</button>
-          <button onClick={buttonClick}>3</button>
+          <button className="num-pad" onClick={buttonClick}>1</button>
+          <button className="num-pad" onClick={buttonClick}>2</button>
+          <button className="num-pad" onClick={buttonClick}>3</button>
           <button onClick={buttonClick}>+</button>
         </div>
         <div className="row">
-          <button onClick={buttonClick}>+/-</button>
-          <button onClick={buttonClick}>0</button>
-          <button onClick={buttonClick}>.</button>
-          <button onClick={buttonClick}>=</button>
+          <button className="num-pad" onClick={buttonClick}>+/-</button>
+          <button className="num-pad" onClick={buttonClick}>0</button>
+          <button className="num-pad" onClick={buttonClick}>.</button>
+          <button className="equal-button" onClick={buttonClick}>=</button>
         </div>
         
       </div>
